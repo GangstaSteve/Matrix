1885a877758cf61356031de63fb1a6ded21da1d5
GangstaSteve <49599379+GangstaSteve@users.noreply.github.com>
2019-04-23 19:37:31 -0400

    Create Matrix.py

---
+++ Matrix.py
@@ -0,0 +1,23 @@
+import os
+import time
+import colorama
+import random
+import winsound
+colorama.init()
+os.system("mode 158")
+print(colorama.Fore.GREEN+"I've been watching you.")
+input("Press any key to continue . . . ")
+os.system("cls")
+print("You are the one.")
+input("Press any key to continue . . . ")
+os.system("cls")
+print("This . . . ")
+input("Press any key to continue . . . ")
+os.system("cls")
+print("This is the Matrix.")
+input("Press any key to continue . . . ")
+winsound.Beep(1000,2500)
+os.system("cls")
+while True:
+    print(colorama.Fore.GREEN+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999))+str(random.randint(100000,999999)))
+    time.sleep(0.1)
